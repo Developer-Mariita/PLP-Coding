d5f4999ba9798c161e3f9ea91b70585c6932afef
Laban Marita <labanmarita@gmail.com>
2025-08-14 21:23:24 +0300

    Add files via upload

---
+++ calculator_assignment.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+
+difference_result = num1 - num2
+
+product_result = num1 * num2
+
+quotient_result = num1 / num2
+
+print("Results of your two numbers:")
+print(f"{num1} + {num2} = {sum_result}")
+print(f"{num1} - {num2} = {difference_result}")
+print(f"{num1} * {num2} = {product_result}")
+print(f"{num1} / {num2} = {quotient_result}") 

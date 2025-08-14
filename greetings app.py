becca5caa4d8794c949a90a7ed66a6a9e779238c
Laban Marita <labanmarita@gmail.com>
2025-08-14 21:39:03 +0300

    Add files via upload

---
+++ greetings app.py
@@ -0,0 +1,7 @@
+name=str(input("What is your user name? "))
+print(name)
+
+favourite_color=str(input("What is your favourite color? "))
+print(favourite_color)
+
+print("Hello, " + name + "! "+ "Your favourite color, "+ favourite_color ) 
